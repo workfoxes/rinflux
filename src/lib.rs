9276d61db459abf574841eeee7ffa7f17ac43ca8
Vasanth Kumar <itsparser@gmail.com>
2022-01-02 10:23:18 +0530

    Added the test case file for integration testing

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,3 @@ pub use client::options::Option;
 pub use client::options::write::WriteOption;
 pub use client::options::http::HttpOption;
 pub use error::Error;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
